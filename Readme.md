53141f0e77d175bd763234570e4154983546b92f
谭九鼎 <109224573@qq.com>
2023-03-28 17:16:52 +0800

    Remove stream, build QUIC branch

--- Readme.md
+++ Readme.md
@@ -2,5 +2,5 @@
 
 * Minimal
 * The binary release in GitHub Actions artifacts can only be used on Debian (series)
-* Runtime package requirements: libpcre3 zlib1g libunwind8
-* Migrate from https://github.com/imba-tjd/boringx
+* Runtime package requirements: libpcre3 zlib1g
+* Support HTTP3, no HTTP2
--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# 未取消的模块：access、gzip、rewrite、proxy、http_limit_req、http_upstream_ip_hash、upstream_least_conn、http_upstream_keepalive、stream_return
-set -ex;
+# 未取消的模块：access、gzip、rewrite、proxy、http_limit_req、http_upstream_keepalive
+set -ex
 
-NGINX_VERSION=${NGINX_VERSION:-1.20.2};
+# NGINX_VERSION=${NGINX_VERSION:-1.20.2}
+
 CONFIG="\
     --prefix=/etc/nginx \
     --sbin-path=/usr/sbin/nginx \
@@ -17,11 +18,7 @@ CONFIG="\
     --with-pcre-jit \
     --with-http_ssl_module \
     --with-http_realip_module \
-    --with-http_v2_module \
-    --with-stream \
-    --with-stream_ssl_module \
-    --with-stream_ssl_preread_module \
-    --with-stream_realip_module \
+    --with-http_v3_module \
     --without-select_module \
     --without-poll_module \
     --without-http_charset_module \
@@ -49,23 +46,20 @@ CONFIG="\
     --without-mail_pop3_module \
     --without-mail_imap_module \
     --without-mail_smtp_module \
-    --without-stream_limit_conn_module \
-    --without-stream_geo_module \
-    --without-stream_map_module \
-    --without-stream_split_clients_module \
-    --without-stream_upstream_hash_module \
-    --without-stream_upstream_random_module \
-    --without-stream_upstream_zone_module \
     --with-openssl-opt=enable-ec_nistp_64_gcc_128 \
-";
-CC_OPTS=--with-cc-opt="-Ofast -fstack-protector -fpic -m64 -flto=thin";
-LD_OPTS=--with-ld-opt="-fpie -pie -Wl,-z,relro -Wl,-z,now -Wl,--as-needed -flto=thin -fuse-ld=lld";
+"
+CC_OPTS=--with-cc-opt="-Ofast -fstack-protector -fpic -m64 -flto=thin"
+LD_OPTS=--with-ld-opt="-fpie -pie -Wl,-z,relro -Wl,-z,now -Wl,--as-needed -flto=thin -fuse-ld=lld"
 
-curl -L https://nginx.org/download/nginx-$NGINX_VERSION.tar.gz | tar xzf -;
-cd ./nginx-$NGINX_VERSION;
-sed -i -e '/NGX_CLANG_OPT="-O"/d' -e '/CFLAGS="$CFLAGS -g"/d' ./auto/cc/clang;
-./configure $CONFIG "$CC_OPTS" "$LD_OPTS";
-make -sj$(nproc);
-strip -s ./objs/nginx;
-upx --lzma ./objs/nginx;
-cp ./objs/nginx ../;
+# curl -L https://nginx.org/download/nginx-$NGINX_VERSION.tar.gz | tar xzf -
+# cd ./nginx-$NGINX_VERSION
+curl -LO https://github.com/nginx-quic/nginx-quic/archive/refs/heads/quic.zip
+unzip -q quic.zip && rm quic.zip && cd nginx-quic-quic
+
+sed -i -e '/NGX_CLANG_OPT="-O"/d' -e '/CFLAGS="$CFLAGS -g"/d' ./auto/cc/clang
+./configure $CONFIG "$CC_OPTS" "$LD_OPTS"
+
+make -sj$(nproc)
+strip -s ./objs/nginx
+upx --lzma ./objs/nginx
+cp ./objs/nginx ../
